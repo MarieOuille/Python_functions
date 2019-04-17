ef5ee143cf8af8a38f254c30d208b1d107076a9c
MarieOuille <34027768+MarieOuille@users.noreply.github.com>
2019-04-17 17:07:56 +0200

    Add files via upload

---
+++ smooth.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 11:39:27 2019
+
+@author: ouille
+"""
+import numpy as np
+
+def smooth(a,WSZ):
+    # a: NumPy 1-D array containing the data to be smoothed
+    # WSZ: smoothing window size needs, which must be odd number,
+    # as in the original MATLAB implementation
+    out0 = np.convolve(a,np.ones(WSZ,dtype=int),'valid')/WSZ    
+    r = np.arange(1,WSZ-1,2)
+    start = np.cumsum(a[:WSZ-1])[::2]/r
+    stop = (np.cumsum(a[:-WSZ:-1])[::2]/r)[::-1]
+    return np.concatenate((  start , out0, stop  ))
